ed8aa67f1b21d16d76bff892ce81c73b83af448e
rocknegi <rocknegi53@gmail.com>
2017-12-30 16:35:17 +0000

    cosmofy

--- cos.mk
+++ cos.mk
@@ -18,19 +18,23 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common Cardinal-AOSP stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := cardinal_kenzo
+PRODUCT_NAME := cos_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Official
+ KBUILD_BUILD_HOST := Saturn
+ KBUILD_BUILD_USER := Savitar
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
